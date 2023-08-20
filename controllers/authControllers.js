aa621887ff0cc454557c6eda295e8018b6816034
aicha <aichaaichabff@gmail.com>
2023-08-20 14:08:37 +0000

    first commit

---
+++ controllers/authControllers.js
@@ -0,0 +1,164 @@
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, adress, answer } = req.body;
+
+    //validations
+    if (!name) {
+      return res.send({ message: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone is Required" });
+    }
+    if (!adress) {
+      return res.send({ message: "Adress is Required" });
+    }
+    if (!answer) {
+      return res.send({ message: "answer is Required" });
+    }
+
+    //check user
+    const existingUser = await userModel.findOne({ email });
+    //existiong user
+    if (existingUser) {
+      res.status(200).send({
+        success: false,
+        message: "Already Register please login",
+      });
+    }
+
+    //register user
+    const hashedPassword = await hashPassword(password);
+
+    //save
+    const user = new userModel({
+      name,
+      email,
+      phone,
+      adress,
+      answer,
+      password: hashedPassword,
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+};
+
+//POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registred",
+      });
+    }
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "Login successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        adress: user.adress,
+        answer: user.answer,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
+};
+
+//forcot password
+
+export const forgotPasswordController = async (req, res) => {
+  try {
+    const { email, answer, newPassword } = req.body;
+    if (!email) {
+      res.status(400).send({ message: "Email is required" });
+    }
+    if (!answer) {
+      res.status(400).send({ message: "answer is required" });
+    }
+    if (!newPassword) {
+      res.status(400).send({ message: "newPassword is required" });
+    }
+
+    //check user
+    const user = await userModel.findOne({ email, answer });
+
+    //validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "wrong email or answer",
+      });
+    }
+    const hashed = await hashPassword(newPassword);
+    await userModel.findByIdAndUpdate(user._id, { password: hashed });
+    res.status(200).send({
+      success: true,
+      message: "Password Reset Successfully ! ",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).sen({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
+
+//test controller
+
+export const testController = (req, res) => {
+  res.send("protected Route");
+};
---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+// const colors=require("colors")
+import morgan from "morgan"
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
+import cors from "cors"
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth',authRoutes);
+
+//rest api
+app.get("/", (req, res) => {
+  //   res.send({
+  //     message: "Welcome to ecommerce app",
+  //   });
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+//port
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON ${process.env.DEV_MODE} MODE ON PORT ${PORT}!`);
+});
